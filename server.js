fadbc421d1f334baf9f05e9fe1ebf15809a39888
eliaspaul04 <eliaspaul.k2004@gmail.com>
2024-12-08 19:33:44 +0400

    get request

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const path = require('path');
+const MongoClient = require('mongodb').MongoClient;
 const app = express();
 const PORT = 3000;
+
+// Middleware for JSON parsing
+app.use(express.json());
+
+// Setting CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers'
+    );
+    next();
+});
 
 // Serve static files from the current directory
 app.use(express.static(path.join(__dirname)));
@@ -11,29 +27,46 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Start the server
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+// MongoDB connection
+let db;
+const uri = 'mongodb+srv://ep04:eliaspaul2004@cluster0.iotzr.mongodb.net/'; // Use environment variables for sensitive data
+
+MongoClient.connect(uri)
+    .then(client => {
+        db = client.db('webstore'); // Replace 'webstore' with your actual database name
+        console.log('Connected to MongoDB');
+    })
+    .catch(err => {
+        console.error('Error connecting to MongoDB:', err.message);
+    });
+
+// Middleware for handling collection name in the route
+app.param('collectionName', (req, res, next, collectionName) => {
+    if (!db) {
+        return res.status(500).send('Database not initialized');
+    }
+    req.collection = db.collection(collectionName);
+    next();
 });
+
+// API route to fetch products
+app.get('/products', (req, res, next) => {
+    if (!db) {
+        return res.status(500).send('Database not connected');
+    }
+    db.collection('products')
+        .find({})
+        .toArray()
+        .then(results => res.send(results))
+        .catch(err => next(err));
+});
+
 
 app.use((req, res) => {
     res.status(404).send('Page not found');
 });
 
-//setting cors
-app.use ((req,res,next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-
-    next();
-})
-
-// Mongodb connection
-const MongoClient = require('mongodb').MongoClient;
-
-let db; 
-MongoClient.connect('mongodb+srv://ep04:eliaspaul2004@cluster0.iotzr.mongodb.net/', (err, client) => {
-    db = client.db('webstore')
-})
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
